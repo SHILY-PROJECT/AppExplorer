d5aa8646b706328d40888897029fa67ff295d840
SHILY <45015327+SHILY-PROJECT@users.noreply.github.com>
2021-11-02 15:32:18 +0300

    -Добавление методов расширения для дочерних моделей "ExplorerObjectViewModel"

---
+++ ExplorerApp/Models/ExplorerObjectViewModelExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExplorerApp.Models
+{
+    public static class ExplorerObjectViewModelExtension
+    {
+        public static List<ExplorerObjectViewModel> ExtractExplorerObjects(this FolderObjectViewModel folderObject)
+            => folderObject.Folders.Cast<ExplorerObjectViewModel>().Concat(folderObject.Files.Cast<ExplorerObjectViewModel>()).ToList();
+
+        public static List<ExplorerObjectViewModel> GetExplorerObjectsByRoute(this List<FolderObjectViewModel> directoriesList, string route)
+        {
+            var folderObject = directoriesList.FirstOrDefault(x => x.Route == route);
+            if (folderObject == null) return null;
+            return folderObject.ExtractExplorerObjects();
+        }
+
+        public static List<ExplorerObjectViewModel> GetExplorerObjectsByIndex(this List<FolderObjectViewModel> directoriesList, int index)
+        {
+            if (index < 0 || index > (directoriesList.Count - 1)) return null;
+            var folderObject = directoriesList[index];
+            return folderObject.ExtractExplorerObjects();
+        }
+
+    }
+}
